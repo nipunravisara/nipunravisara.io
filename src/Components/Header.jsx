94e83e992e3f85b19468a6b96c3f9ad8e332c0c6
Nipun Ravisara <ravisaranipun97@gmail.com>
2021-05-24 18:09:11 +0530

    New cv

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Pd from "../assets/resume.pdf";
+import PDF from "../assets/nipun-ravisara.pdf";
 import Logo from "../assets/logo";
 
 const Header = () => (
@@ -8,7 +8,7 @@ const Header = () => (
       <Logo />
     </center>
     <div className="fixed top-10 right-10 bg-green-400 px-4 py-2 pt-3 rounded-md">
-      <a className="text-black" href={Pd} target="_blank" rel="noreferrer">
+      <a className="text-black" href={PDF} target="_blank" rel="noreferrer">
         Resume
       </a>
     </div>
