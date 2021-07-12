7bd11bd3ccd80a94e56ef4dca9710ed546d030ca
ravisara <ravisaranipun97@gmail.com>
2021-07-12 14:20:13 +0530

    resume button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => (
     <center>
       <Logo />
     </center>
-    <div className="fixed top-10 right-10 bg-green-400 px-4 py-2 pt-3 rounded-md">
-      <a className="text-black" href={PDF} target="_blank" rel="noreferrer">
+    <div className="fixed top-10 right-10 px-4 py-1 pt-2 border rounded">
+      <a className="text-white" href={PDF} target="_blank" rel="noreferrer">
         Resume
       </a>
     </div>
